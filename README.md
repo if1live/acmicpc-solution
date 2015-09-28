a1468aba9110eb0d8950a784ddfb7eaabe48c747
if1live <libsora25@gmail.com>
2015-09-28 02:25:09 -0400

    advanced test runner

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # acmicpc-solution
 My solution for Baekjoon Online Judge (acmicpc.net)
+[acmicpc.net](https://www.acmicpc.net/)
+
+## How to run?
+```
+sudo apt-get install scm racket colordiff
+```
+
+### Test all problems
+```
+./run.sh test all
+```
+
+### Test specific problem
+```
+./run.sh test 1000
+```
+
+### Run all problems
+```
+./run.sh run_racket all
+```
+
+### Run specific problem
+```
+./run.sh run_scm 1000
+```
+
--- run.sh
+++ run.sh
@@ -1,8 +1,68 @@
 #!/bin/bash
 
-for problem in $(ls | grep "[[:digit:]]")
-do
+function test_all {
+	for problem in $(ls | grep "[[:digit:]]"); do
+		test_problem
+	done
+}
+
+function test_problem {
+	ACTUAL_RACKET="$problem/actual-racket.txt"
+	ACTUAL_SCM="$problem/actual-scm.txt"
+	INPUT="$problem/input.txt"
+	EXPECTED="$problem/expected.txt"
+	SOURCE="$problem/main.rkt"
+	SUCCESS=true
+
 	echo "Problem #$problem"
-	cd $problem ; make ; cd -
-	echo ""
-done
+
+	racket -r $SOURCE < $INPUT > $ACTUAL_RACKET
+	colordiff -w $ACTUAL_RACKET $EXPECTED
+	if [[ $? != "0" ]]; then SUCCESS=false; fi
+
+	scm $SOURCE < $INPUT > $ACTUAL_SCM
+	colordiff -w $ACTUAL_SCM $EXPECTED
+	if [[ $? != "0" ]]; then SUCCESS=false; fi
+
+	rm -rf $ACTUAL_SCM
+	rm -rf $ACTUAL_RACKET
+
+	if [ $SUCCESS = true ]; then echo "Solved!"; fi
+}
+
+function run_racket {
+	ACTUAL_RACKET="$problem/actual-racket.txt"
+	INPUT="$problem/input.txt"
+	SOURCE="$problem/main.rkt"
+	racket -r $SOURCE < $INPUT
+}
+
+function run_scm {
+	ACTUAL_SCM="$problem/actual-scm.txt"
+	INPUT="$problem/input.txt"
+	SOURCE="$problem/main.rkt"
+	scm $SOURCE < $INPUT
+}
+
+if [ "$#" -ne 2 ]; then
+	echo "Commands"
+	echo "$0 test all"
+	echo "$0 test 1000"
+	echo "$0 run_racket 1000"
+	echo "$0 run_scm 1000"
+	exit -1
+fi
+
+problem=$2
+
+if [ $1 = "test" ]; then
+	if [ $2 = "all" ]; then
+		test_all
+	else
+		test_problem
+	fi
+elif [ $1 = "run_racket" ]; then
+	run_racket
+elif [ $1 = "run_scm" ]; then
+	run_scm
+fi
